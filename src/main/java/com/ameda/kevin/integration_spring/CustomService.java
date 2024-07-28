83db48fcf88152f59f6d9a0e19163a49f1f118b0
ameda-kevin <amedakevin@gmail.com>
2024-07-28 15:11:49 +0300

    payload enricher with some notes

---
+++ src/main/java/com/ameda/kevin/integration_spring/CustomService.java
@@ -0,0 +1,15 @@
+package com.ameda.kevin.integration_spring;/*
+*
+@author ameda
+@project integration-spring
+*
+*/
+
+public class CustomService {
+
+
+    public String getPhoneNumber(Person person){
+        //contact data store...
+        return "078929633638";
+    }
+}
