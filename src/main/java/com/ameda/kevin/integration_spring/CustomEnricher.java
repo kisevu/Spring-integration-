7b44d10b5d71448eb3acb285cacb8dba6406a12a
ameda-kevin <amedakevin@gmail.com>
2024-07-28 14:52:07 +0300

    Header enricher done

---
+++ src/main/java/com/ameda/kevin/integration_spring/CustomEnricher.java
@@ -0,0 +1,14 @@
+package com.ameda.kevin.integration_spring;
+/*
+*
+@author ameda
+@project integration-spring
+*
+*/
+
+public class CustomEnricher {
+
+    public String getHeaderValue(){
+        return "This is the custom header value enriched";
+    }
+}
