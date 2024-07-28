beffd7ded0a73453cf350cb98d36de35f6176715
ameda-kevin <amedakevin@gmail.com>
2024-07-28 10:04:06 +0300

    filters

--- src/main/java/com/ameda/kevin/integration_spring/IntegrationSpringApplication.java
+++ src/main/java/com/ameda/kevin/integration_spring/IntegrationSpringApplication.java
@@ -1,18 +1,16 @@
 package com.ameda.kevin.integration_spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Primary;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @SpringBootApplication
 @Configuration
@@ -30,22 +28,9 @@ public class IntegrationSpringApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) {
-		List<Employee> employees = new ArrayList<>();
-		employees.add(new Employee(1,"kevin",1500.00));
-		employees.add(new Employee(2,"oscar",2000.00));
-		employees.add(new Employee(1,"judith",1000.00));
-
-
-//		for(int x = 0; x< employees.size();++x){
-//			Employee employee = employees.get(x);
-//			Message<?> message = MessageBuilder
-//					.withPayload(employee)
-//					.build();
-//			this.gateway.print(message);
-//		}
-		for(int x = 0; x<employees.size(); x++){
+		for(int x = 0; x<10; x++){
 			Message<?> message = MessageBuilder
-					.withPayload("kev")
+					.withPayload(x)
 					.build();
 			this.gateway.print(message);
 		}
