deb25d5de127a60fb18ea4d1436d0a650f415d29
ameda-kevin <amedakevin@gmail.com>
2024-07-29 01:44:02 +0300

    inbound channel adapter reading off file the file system

---
+++ src/main/java/com/ameda/kevin/integration_spring/FilePrinter.java
@@ -0,0 +1,34 @@
+package com.ameda.kevin.integration_spring;/*
+*
+@author ameda
+@project integration-spring
+*
+*/
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FilePrinter {
+
+    public void print(File file){
+        BufferedReader reader=null;
+       try{
+           reader = new BufferedReader(new FileReader(file));
+           String line ="";
+           while((line=reader.readLine())!=null){
+               System.out.println(line); //display on console
+           }
+       }catch (Exception e){
+           e.printStackTrace();
+       }finally {
+           try{
+               reader.close(); //close resources
+           }catch (IOException ex){
+               ex.printStackTrace();
+           }
+       }
+    }
+
+}
